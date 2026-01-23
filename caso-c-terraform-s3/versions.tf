487faf474fe4129caa1f4093edf151b065be3dcc
Vladimir Acuña <vladimir.acuna.dev@gmail.com>
2026-01-23 18:53:25 -0300

    feat(terraform): configurar remote backend s3 para persistencia de estado

--- caso-c-terraform-s3/versions.tf
+++ caso-c-terraform-s3/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.14.0"
 
+  backend "s3" {
+    bucket = "vladimir-terraform-state-2026"
+    key    = "caso-c/terraform.tfstate"
+    region = "sa-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
